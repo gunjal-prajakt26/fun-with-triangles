5afb937bc8416415e96903b95f52ed81bf9cde76
gunjal-prajakt26 <gunjal.prajakt951@gmail.com>
2022-02-10 22:58:07 +0530

    hypoenious text updated

---
+++ hypotenious/hypotenious-app.js
@@ -0,0 +1,14 @@
+var firstSideInput = document.querySelector(".side-1");
+var secondsideInput = document.querySelector(".side-2");
+
+var checkBtn = document.querySelector(".btn-calculate");
+var outputDisplay = document.querySelector(".output");
+
+
+checkBtn.addEventListener('click', function calculateHypotenious(){
+    const a = firstSideInput.value;
+    const b = secondsideInput.value;
+    const sum = a*a + b*b;
+    const c = Math.sqrt(sum);
+    outputDisplay.innerText= "Hypotenious is: "+ c.toFixed(2);
+});
