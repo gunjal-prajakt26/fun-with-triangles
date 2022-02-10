b5d972b6b8c8ea393e422f79a6a1052c5bc4993a
gunjal-prajakt26 <gunjal.prajakt951@gmail.com>
2022-02-10 22:44:00 +0530

    quiz text updated

---
+++ quiz-app/quiz-app.js
@@ -0,0 +1,22 @@
+var formContainer = document.querySelector(".form-container");
+var submitBtn = document.querySelector("#submit-btn");
+var output = document.querySelector(".output-display");
+
+const  correctAnswers =["90°", "right angled", "An angle that measures less than 90", "3", "Equilateral", "Equilateral", "Scalene", "Scalene", "acute", "obtuse"];
+        
+submitBtn.addEventListener('click', function calculateScore(){
+    const myData = new FormData(formContainer);
+
+    let score = 0;
+    let index = 0;
+
+    for(let data of myData.values()){
+       
+        if(data === correctAnswers[index]){
+            score= score + 1  ;
+       }
+       index= index + 1 ;
+    }
+    output.style.dispay="block";
+    output.innerText = "Your Score is: " + score;
+});
