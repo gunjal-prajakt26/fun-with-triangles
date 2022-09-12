56798a290a5ec8d9df348ae60a2bc50c88c7798e
gunjal-prajakt26 <gunjal.prajakt951@gmail.com>
2022-09-12 14:34:16 +0530

    quiz fun updated

--- quiz-app/quiz-app.js
+++ quiz-app/quiz-app.js
@@ -17,6 +17,6 @@ submitBtn.addEventListener('click', function calculateScore(){
        }
        index= index + 1 ;
     }
-    outputDisplay.style.border= "2px solid #475569";
+    output.style.border= "2px solid #475569";
     output.innerText = "Your Score is: " + score;
 });
